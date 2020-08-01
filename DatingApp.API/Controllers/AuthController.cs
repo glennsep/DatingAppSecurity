4efb4c52b0c801a713ad64d9372be475c3eaa91b
Glenn Seplowitz <glennsepaws@optonline.net>
2020-08-01 15:27:52 -0400

    Section 20.209

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -67,7 +67,7 @@ namespace DatingApp.API.Controllers
 
                 return Ok(new
                 {
-                    token = GenerateJwtToken(user),
+                    token = GenerateJwtToken(user).Result,
                     user = appUser
                 });
             }

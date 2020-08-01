67eb5b9a36714238f23573b44a21d45d0cf14d92
Glenn Seplowitz <glennsepaws@optonline.net>
2020-08-01 15:50:46 -0400

    Section 20.211

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -49,7 +49,7 @@ export const appRoutes: Routes = [
       },
       {
         path: 'admin',
-        component: AdminPanelComponent
+        component: AdminPanelComponent, data: {roles: ['Admin', 'Moderator']}
       }
     ]
   },

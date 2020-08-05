dc6460b5690bf50d451d5980a701e575691b4024
Glenn Seplowitz <glennsepaws@optonline.net>
2020-08-05 15:09:30 -0400

    Section 20.216

--- DatingApp-SPA/src/app/admin/roles-modal/roles-modal.component.ts
+++ DatingApp-SPA/src/app/admin/roles-modal/roles-modal.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap';
 import { User } from 'src/app/_models/user';
+
 
 @Component({
   selector: 'app-roles-modal',
@@ -8,6 +9,7 @@ import { User } from 'src/app/_models/user';
   styleUrls: ['./roles-modal.component.css']
 })
 export class RolesModalComponent implements OnInit {
+  @Output() updateSelectedRoles = new EventEmitter();
 
   user: User;
   roles: any[];
@@ -15,6 +17,11 @@ export class RolesModalComponent implements OnInit {
   constructor(public bsModalRef: BsModalRef) {}
 
   ngOnInit() {
+  }
+
+  updateRoles() {
+    this.updateSelectedRoles.emit(this.roles);
+    this.bsModalRef.hide();
   }
 
 }

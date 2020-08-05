0d1b0c1450ef6dd5ab546fdb77f53d72c62deb28
Glenn Seplowitz <glennsepaws@optonline.net>
2020-08-05 14:35:45 -0400

    Section 20.215

--- DatingApp-SPA/src/app/admin/user-management/user-management.component.ts
+++ DatingApp-SPA/src/app/admin/user-management/user-management.component.ts
@@ -28,18 +28,45 @@ export class UserManagementComponent implements OnInit {
     });
   }
 
-  editRolesModal() {
+  editRolesModal(user: User) {
     const initialState = {
-      list: [
-        'Open a modal with component',
-        'Pass your data',
-        'Do something else',
-        '...'
-      ],
-      title: 'Modal with component'
+      user,
+      roles: this.getRolesArray(user)
     };
     this.bsModalRef = this.modalService.show(RolesModalComponent, {initialState});
     this.bsModalRef.content.closeBtnName = 'Close';
+  }
+
+  private getRolesArray(user) {
+    const roles = [];
+    const userRoles = user.roles;
+    const availableRoles: any[] = [
+      {name: 'Admin', value: 'Admin'},
+      {name: 'Moderator', value: 'Moderator'},
+      {name: 'Member', value: 'Member'},
+      {name: 'VIP', value: 'VIP'}
+    ];
+
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < availableRoles.length; i++) {
+      let isMatch = false;
+      // tslint:disable-next-line: prefer-for-of
+      for (let j = 0; j < userRoles.length; j++) {
+        if (availableRoles[i].name === userRoles[j]) {
+          isMatch = true;
+          availableRoles[i].checked = true;
+          roles.push(availableRoles[i]);
+          break;
+        }
+      }
+      if (!isMatch) {
+        availableRoles[i].checked = false;
+        roles.push(availableRoles[i]);
+      }
+    }
+
+    return roles;
+
   }
 
 }
